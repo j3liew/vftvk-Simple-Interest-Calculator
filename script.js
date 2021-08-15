8336ab4348a63b03ce5d9e7199a0fc9130141bb5
j3liew <joash.llt99@gmail.com>
2021-08-15 16:26:33 +0800

    Changes from GitBash

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
     
+    document.getElementByID("result").innerText="If you deposit"+principal+",/n at an interest of "+rate+"%./n You will receive an amount of "+interest+",/n in the year "+year;
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
