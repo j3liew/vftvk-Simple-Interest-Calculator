f9c72e5393d3de26b68392d31963e43db6691894
j3liew <joash.llt99@gmail.com>
2021-08-17 22:37:32 +0800

    Changes from VSCode

--- script.js
+++ script.js
@@ -7,11 +7,29 @@ function compute()
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    document.getElementByID("result").innerText="If you deposit"+principal+",/n at an interest of "+rate+"%./n You will receive an amount of "+interest+",/n in the year "+year;
+    var principalHTML = "<br> If you deposit <span class='highlight'>"+principal+"</span>";
+    var rateHTML = ",<br>at an interest of <span class='highlight'>"+rate+"%</span>";
+    var interestHTML = ",<br>you will receive an amount of <span class='highlight'>"+interest+"</span>";
+    var yearHTML = ",<br>in the year <span class='highlight'>"+year+"</span>";
+
+    document.getElementById("result").innerHTML = principalHTML+rateHTML+interestHTML+yearHTML;
 }
 
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+function checkPrincipal()
+{
+    var principal = document.getElementById("principal").value;
+    if ((principal == '') || (principal <= '0'))
+    {
+        alert("Enter a positive number.");
+        principal.focus();
+        return false;
+    }
+    
+    return true;
 }
